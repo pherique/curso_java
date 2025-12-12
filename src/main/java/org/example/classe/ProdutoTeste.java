49fa94c45e38a9e3123d4a84e061a791a014b5e2
Pedro <phenrique.sduarte@gmail.com>
2025-12-12 15:51:09 -0300

    Salvar

--- src/main/java/org/example/classe/ProdutoTeste.java
+++ src/main/java/org/example/classe/ProdutoTeste.java
@@ -1,8 +1,29 @@
 package org.example.classe;
 
+import java.util.Scanner;
+
 public class ProdutoTeste {
 
     static void main() {
 
+       // int a = 3;
+       // Scanner entrada = new Scanner(System.in);
+
+        Produto p1 = new Produto();
+        p1.nome = "Notebook";
+        p1.preco = 4356.89;
+        p1.desconto = 0.25;
+
+        var p2 = new Produto();
+        p2.nome = "Caneta Preta";
+        p2.preco = 12.56;
+        p2.desconto = 0.29;
+
+        System.out.println(p1.nome);
+        System.out.println(p2.nome);
+
+        double precoFinal1= p1.preco * (1 - p1.desconto);
+        double precoFinal2 = p2.preco * (1 - p2.desconto);
+        System.out.println((precoFinal1 + precoFinal2) / 2);
     }
 }
