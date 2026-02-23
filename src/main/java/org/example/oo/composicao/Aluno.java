fb8c51138c35b2c541c7ac9422d241da7510f9ea
Pedro <phenrique.sduarte@gmail.com>
2026-02-23 14:33:20 -0300

    Salvar

--- src/main/java/org/example/oo/composicao/Aluno.java
+++ src/main/java/org/example/oo/composicao/Aluno.java
@@ -1,4 +1,21 @@
 package org.example.oo.composicao;
 
+import java.util.ArrayList;
+
 public class Aluno {
+
+    String nome;
+    ArrayList<Curso> cursos = new ArrayList<>();
+
+    Aluno(String nome){
+        this.nome = nome;
+    }
+
+    void adicionarCurso(Curso curso){
+        this.cursos.add(curso);
+        curso.alunos.add(this);
+    }
+    public String toString(){
+        return nome;
+    }
 }
